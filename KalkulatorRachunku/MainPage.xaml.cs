140e0aca9a82f86bd31fcf92e12eb1d22966fb18
Filip Celejewski <FilipCelejewski@wp.pl>
2025-03-16 14:44:30 +0100

    Dodaj pliki projektów.

---
+++ KalkulatorRachunku/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace KalkulatorRachunku
+{
+    public partial class MainPage : ContentPage
+    {
+        int billAmount = 0;
+        int guests = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BillAmountChanged(object sender, EventArgs e)
+        {
+            billAmount = int.Parse(billAmountEntry.Text);
+        }
+
+        private void GuestsChanged(object sender, EventArgs e)
+        {
+            guests = int.Parse(GuestsEntry.Text);
+        }
+
+        private void TipSliderChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ KalkulatorRachunku/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace KalkulatorRachunku;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
